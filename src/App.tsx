80b1fd9cbcd8f9494bc2af87db88a0bb326d245f
GuyNoumsi <guyjuniornoumsi5@gmail.com>
2025-01-31 07:33:05 -0500

    Responsive nav bar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-
-import { Button } from '@chakra-ui/react'
-import './App.css'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
-
-  return <Button colorScheme = 'green'> Button </Button>
-
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`, // 1024px upward
+        }}
+      >
+        <GridItem area="nav" bg="green">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="red">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="yellow">
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
-export default App
+export default App;
