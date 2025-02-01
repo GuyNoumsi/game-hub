5ebdf553b6fd68fc592f75bf279bf41937385014
GuyNoumsi <guyjuniornoumsi5@gmail.com>
2025-02-01 15:22:04 -0500

    useGames hook added

---
+++ src/hooks/useGames.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/apiClient";
+import { CanceledError } from "axios";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface GameApiResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controlller = new AbortController();
+
+    apiClient
+      .get<GameApiResponse>("/games", { signal: controlller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controlller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
